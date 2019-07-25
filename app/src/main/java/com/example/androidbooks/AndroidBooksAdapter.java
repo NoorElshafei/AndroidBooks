a4ef0fe770db0af19cabd2dc35257d5048973ca5
noor <noor.elshafie@gmail.com>
2019-07-25 07:50:23 +0200

    update xml and update adpter class and class model

--- app/src/main/java/com/example/androidbooks/AndroidBooksAdapter.java
+++ app/src/main/java/com/example/androidbooks/AndroidBooksAdapter.java
@@ -1,4 +1,53 @@
 package com.example.androidbooks;
 
-public class AndroidBooksAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class AndroidBooksAdapter extends ArrayAdapter<AndroidBooksModel> {
+
+
+    public AndroidBooksAdapter( Context context, int resource, List<AndroidBooksModel> objects) {
+        super(context, resource, objects);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listview =convertView;
+        if(listview==null){
+            listview= LayoutInflater.from(getContext()).inflate(
+                    R.layout.book_item,parent,false);
+        }
+        AndroidBooksModel currentBook=getItem(position);
+
+        TextView nameBookTextView= listview.findViewById(R.id.name_book_text_view);
+        TextView authorBookTextView= listview.findViewById(R.id.author_text_view);
+        TextView languageBookTextView= listview.findViewById(R.id.language_text_view);
+        TextView currencyBookTextView= listview.findViewById(R.id.currency_text_view);
+        TextView priceBookTextView= listview.findViewById(R.id.price_text_view);
+        ImageView bookImageView=listview.findViewById(R.id.book_image_view);
+
+        nameBookTextView.setText(currentBook.getBookName());
+        authorBookTextView.setText(currentBook.getBookAuthor());
+        languageBookTextView.setText(currentBook.getBookLanguage());
+        currencyBookTextView.setText(currentBook.getBookCurrency());
+        priceBookTextView.setText(""+currentBook.getBookPrice());
+        Picasso.get().load(currentBook.getBookPic()).into(bookImageView);
+
+
+
+    }
 }
--- app/src/main/java/com/example/androidbooks/AndroidBooksModel.java
+++ app/src/main/java/com/example/androidbooks/AndroidBooksModel.java
@@ -1,4 +1,39 @@
 package com.example.androidbooks;
 
 public class AndroidBooksModel {
+    private String bookName,bookPic,bookLanguage,bookAuthor,bookCurrency;
+    private double bookPrice;
+
+    public AndroidBooksModel(String bookName, String bookPic, String bookLanguage, String bookAuthor, double bookPrice,String bookCurrency) {
+        this.bookName = bookName;
+        this.bookPic = bookPic;
+        this.bookLanguage = bookLanguage;
+        this.bookAuthor = bookAuthor;
+        this.bookPrice = bookPrice;
+        this.bookCurrency=bookCurrency;
+    }
+
+    public String getBookCurrency() {
+        return bookCurrency;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getBookPic() {
+        return bookPic;
+    }
+
+    public String getBookLanguage() {
+        return bookLanguage;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public double getBookPrice() {
+        return bookPrice;
+    }
 }
